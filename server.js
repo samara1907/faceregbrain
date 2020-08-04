86e722e1537d2868679db8eb612e336ccee8f8dd
samara1907 <67167402+samara1907@users.noreply.github.com>
2020-08-04 17:09:53 +0200

    adding port

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ app.get('/profile/:id', (req,res)=>{profile.handleProfileGet(req,res,db)})
 app.put('/image' , (req,res)=>{image.handleImage(req,res,db)})
 app.post('/imageapi' , (req,res)=>{image.handleApiCall(req,res)})
 
-app.listen(3000, ()=>{
-	console.log('done');
+app.listen(process.env.PORT || 3000, ()=>{
+	console.log(`done ${process.env.PORT}`);
 })
