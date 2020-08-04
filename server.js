de1d4d5975a4e130a2f9ff2cb8ba3ee60a5f5255
samara1907 <67167402+samara1907@users.noreply.github.com>
2020-08-04 18:55:19 +0200

    new shit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-app.get('/', (req,res)=>{res.send(`now working...`)})
+app.get('/', (req,res)=>{res.send(`now bdaaaaaan...`)})
 
 app.post('/signin', (req,res)=>{signin.handleSignin(req, res, db, bcrypt)})
 
